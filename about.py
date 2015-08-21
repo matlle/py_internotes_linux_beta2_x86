594345d0c00bdebe514d108a0a0ddf0fef8108d1
Martial Babo <paso.175@gmail.com>
2015-08-21 03:47:21 +0000

    Another little commit

--- about.py
+++ about.py
@@ -37,7 +37,7 @@ class About(QDialog):
            u"<strong>Version 1.0.2 (Windows x86)</strong><br></br><br/><br/><br/>"
            u"Copyright © 2015 Matlle e.i. Tous droits reservés.<br/><br/><br/><br/><br/><br/><br/><br/>"
            u"<span style="">(+225) 07 08 68 98 / 41 87 07 68 / 01 58 03 30</span><br/>"
-           u"matllesoftware@gmail.com"
+           u"matllesoftware@gmail.com<br/>"
            u"www.matlle.com"))
 
         label_about.setAlignment(Qt.AlignLeft)
@@ -52,19 +52,44 @@ class About(QDialog):
 
         label_licence = QLabel()
         label_licence.setText(QString(
-            u"Licence d'utilisation"
-            u""))
+            u"<h4>1. Licence<h4>"
+            u"Le Propriétaire accorde au Bénéficiaire une licence non exclusive et non transférable "
+            u"(1) pour utiliser le logiciel uniquement dans le cadre de ses activités à l'emplacement "
+            u"et au sein de l'environnement prévu et (2) une copie du logiciel aux fins d'archivage ou de "
+            u"sauvegarde pourvu que les mentions des titres, droits de marques et droits d'auteurs, "
+            u"periode d'utilisation et autres droits personnels ou réservés figurent sur toutes les copies et "
+            u"que toutes les copies soient sujettes au présent contrat. <br/>"
+            u"<h4>2. Distribution</h4>"
+            u"Sauf stipulation explicite contenue dans ce présent contrat, le Bénéficiaire "
+            u"ne devra pas : (1) mettre à disposition d'autrui ni distribuer le logiciel ni aucun élément du "
+            u"logiciel afférent à quelque partie que ce soit, ni accorder une sous-licence; (2) copier, concevoir "
+            u"une version inverse, identique ou semblable, décomposer, dissocier, modifier en partie ou en totalité "
+            u"le logiciel; (3) utiliser le logiciel dans le cadre d'échange de services, la sous-traitance, ou "
+            u"autoriser une tierce partie à l'environnement prévu d'avoir accès au logiciel.<br/>"
+            u"<h4>3. Installation et acceptation </h4>"
+            u"Le Propriétaire prendra toute les dispositions nécessaires pour livrer les copies du logiciel "
+            u"dont la licence est jointe au présent logiciel. Le Bénéficiaire disposera alors de 15 jours après la livraison "
+            u"pour effectuer le test d'acceptation du logiciel. L'acceptation du logiciel par le Bénéficiaire sera "
+            u"effective à partir de la date d'utilisation du logiciel ou à la date d'expiration de la période "
+            u"de 15 jours à compter de la date d'installation sans que le Bénéficiaire n'ait besoin d'informer le "
+            u"le Propriétaire de quelque erreur. Si le Bénéficiaire notifie une erreur au Propriétaire et le Bénéficiaire "
+            u"diagnostique la prétendue erreur, le logiciel sera accepté à la correction de cette erreur."
+            ))
+
         label_licence.setAlignment(Qt.AlignLeft)
         label_licence.setWordWrap(True)
 
+        scroll_area = QScrollArea()
+        scroll_area.setWidget(label_licence)
+
         layout_licence = QHBoxLayout()
-        layout_licence.addWidget(label_licence)
+        layout_licence.addWidget(scroll_area)
         page_licence.setLayout(layout_licence)
         
 
         onglets = QTabWidget()
         onglets.addTab(page_about, "À propos")
-        onglets.addTab(page_licence, "Licence")
+        onglets.addTab(page_licence, "Contrat de Licence")
 
 
         layout_body = QHBoxLayout()
